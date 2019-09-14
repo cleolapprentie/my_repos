e57e35ea61622a1742de084214192ac0eb609517
kayahino <48889456+kayahino@users.noreply.github.com>
2019-09-15 02:18:20 +0800

    update

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -5,7 +5,11 @@ var app = new Vue({
     initUrl: 'https://api.github.com/users/',
     page: 1,
     repos: [],
-    switchUsername: false
+    checkData: [],
+    switchUsername: false,
+    loading: false,
+    error: false,
+    next: true
   },
   created: function() {
     this.fetchApi()
@@ -19,6 +23,7 @@ var app = new Vue({
   methods: {
     fetchApi: function() {
       var self = this
+      this.loading = true
       var getData = this.initUrl + this.username + '/repos?sort=updated&page=' + this.page + '&per_page=5'
       fetch(getData).then(response => {
           if (response.status !== 200) {
@@ -27,15 +32,18 @@ var app = new Vue({
           return response.json()
         })
         .then(data => {
-          if (data.documentation_url === 'https://developer.github.com/v3/#rate-limiting') {
-            alert('資料讀取錯誤：' + data.message)
-            return
-          }
-          if (this.switchUsername) { this.repos = [] }
-          self.switchUsername = false
-          data.forEach(function(el) {
-            self.repos.push(el)
-          })
+          setTimeout(function() {
+            self.loading = false
+            if (data.documentation_url === 'https://developer.github.com/v3/#rate-limiting') {
+              alert('資料讀取錯誤：' + data.message)
+              self.error = true
+              return
+            }
+            self.switchUsername = false
+            data.forEach(function(el) {
+              self.repos.push(el)
+            })
+          }, 500)
         })
         .then(() => {
           $('[data-toggle="tooltip"]').tooltip();
@@ -56,20 +64,23 @@ var app = new Vue({
       var vm = this
       $(window).on('scroll', function() {
         var scrollPos = $(window).scrollTop()
-        // infinite scroll
-        if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
-          if (vm.switchUsername || !vm.repos.length) { return }
-          vm.page++
-          vm.fetchApi()
-        }
         $('.hero-bg').css('background-position-y', - 250 + (scrollPos / 2) + 'px')
         $('.hero h1').css('transform', 'translateY(' + (scrollPos / 3) + 'px)')
         $('.hero-link').css('transform', 'translateY(' + (scrollPos / 6) + 'px)')
+        // infinite scroll
+        if (window.innerHeight + window.scrollY + 1 >= document.body.scrollHeight) {
+          if (vm.switchUsername || vm.repos.length === 0) { return }
+          vm.page++
+          vm.fetchApi()
+        }
       })
     },
     search: function() {
+      $('html, body').animate({ scrollTop: $('#app').offset().top - 40 })
+      this.page = 1
+      this.repos = []
+      this.next = true
       this.fetchApi()
-      $('body').animate({ scrollTop: 0 })
     }
   }
 });
